b0aadd630d8f82ce8ea9cef45060cfaeaf0b6cdb
franzpc <franzpc@gmail.com>
2016-10-23 19:41:15 -0500

    Add files via upload

---
+++ Extract_raster.py
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------------------------
+# Autor: Antonio Pantoja @PantojaAntonio
+# Fecha: 11/10/2016
+# email: apantoja.ciat@gmail.com
+# Proposito: Extraer con mascara shp o raster, sirve para una sola carpeta ejemplo worldclim
+# Nota: 
+# ---------------------------------------------------------------------------
+
+import arcpy,os
+from arcpy import env
+from arcpy.sa import *
+arcpy.CheckOutExtension("Spatial")
+
+os.system('cls')
+
+
+CarpetaEntrada = raw_input("\nIndique carpeta de entrada (con varios archivos Raster) ")
+ArchivoMascara = raw_input("\nEntrar el archivo mascara, capa raster o archivo shp (Incluyendo extension .shp)")
+CarpetaSalida = raw_input("\ncarpeta salida de los nuevos raster cortados ")
+
+
+
+arcpy.env.workspace = CarpetaEntrada 
+rasters = arcpy.ListRasters("*", "GRID")
+for raster in rasters:
+	print raster
+	arcpy.env.snapRaster = raster
+	corte = CarpetaSalida + "\\" + raster
+
+	outExtractByMask = ExtractByMask(raster, ArchivoMascara)
+	outExtractByMask.save(corte)
+
+print "Proceso terminado !!!"
+print "revisar: " , CarpetaSalida    
